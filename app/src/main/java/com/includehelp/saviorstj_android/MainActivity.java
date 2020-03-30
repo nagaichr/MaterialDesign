278ffc7c085918aaa6d2226d9a1ff9ea72c4c4af
nagaichr <android#07R>
2020-03-30 13:38:15 +0530

    Disable Status Bar

--- app/src/main/java/com/includehelp/saviorstj_android/MainActivity.java
+++ app/src/main/java/com/includehelp/saviorstj_android/MainActivity.java
@@ -1,14 +1,95 @@
 package com.includehelp.saviorstj_android;
 
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    public static final int OVERLAY_PERMISSION_REQ_CODE = 4545;
+    protected CustomViewGroup blockingView = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+
+            if (!Settings.canDrawOverlays(this)) {
+                Toast.makeText(this, "Please give my app this permission!", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + getPackageName()));
+                startActivityForResult(intent, OVERLAY_PERMISSION_REQ_CODE);
+            }
+            else {
+                disableStatusBar();
+            }
+        }
+        else {
+            disableStatusBar();
+        }
+    }
+
+
+
+    protected void disableStatusBar() {
+
+        WindowManager manager = ((WindowManager) getApplicationContext().getSystemService(Context.WINDOW_SERVICE));
+
+        WindowManager.LayoutParams localLayoutParams = new WindowManager.LayoutParams();
+        localLayoutParams.type = WindowManager.LayoutParams.TYPE_SYSTEM_ERROR;
+        localLayoutParams.gravity = Gravity.TOP;
+        localLayoutParams.flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE |
+
+                // this is to enable the notification to receive touch events
+                WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL |
+
+                // Draws over status bar
+                WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN;
+
+        localLayoutParams.width = WindowManager.LayoutParams.MATCH_PARENT;
+        localLayoutParams.height = (int) (40 * getResources().getDisplayMetrics().scaledDensity);
+        localLayoutParams.format = PixelFormat.TRANSPARENT;
+
+        blockingView = new CustomViewGroup(this);
+        manager.addView(blockingView, localLayoutParams);
+    }
+
+
+    /**
+     * This class creates the overlay on the status bar which stops it from expanding.
+     */
+    public static class CustomViewGroup extends ViewGroup {
+
+        public CustomViewGroup(Context context) {
+            super(context);
+        }
+
+        @Override
+        protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        }
+
+        @Override
+        public boolean onInterceptTouchEvent(MotionEvent ev) {
+            Log.v("customViewGroup", "********** Status bar swipe intercepted");
+            return true;
+        }
+    }
+
+    public void madeLauncher(View view) {
+        Toast.makeText(this, "Jai Ho!!!", Toast.LENGTH_SHORT).show();
     }
 }
